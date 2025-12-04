2126a58c3bd70729c90a4c6343ec44501c945c6f
rolladharani <rolladharani77@gmail.com>
2025-12-04 10:50:49 +0530

    Basic Program 2

---
+++ Promises.js
@@ -0,0 +1,35 @@
+
+
+function s1(id){
+    return new Promise((resolve,reject)=>{
+        console.log("fun1 called");
+        resolve("In this it will get user details",id);
+
+    })
+
+};
+function s2(roll)
+{
+    return new Promise((resolve,reject)=>{
+        console.log("fun2 called");
+        resolve("In this we will get roll number",roll);
+
+    })
+   
+};
+function s3(subid)
+{
+    return new Promise((resolve,reject)=>{
+        console.log("fun3 called");
+        resolve("Here we will get of marks by subid",subid);
+
+    })
+};
+
+s1("123").then((result)=>{
+return s2(result.roll);
+}).then((result)=>{
+    return s3(result.subid)
+}).then((result)=>console.log(result))
+ .catch((error)=>console.log(error));
+
