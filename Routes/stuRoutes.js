ec9687aee3d1b574eee8bc5d8c9bd4aa0ec05f38
rolladharani <rolladharani77@gmail.com>
2026-01-07 23:24:41 +0530

    changes

--- Routes/stuRoutes.js
+++ Routes/stuRoutes.js
@@ -1,7 +1,16 @@
+
 import express from 'express';
 const router = express.Router();
-import {getstudents , addstudents} from "../controllers/stucontrollers.js";
+import {getStudentsDetails,addStudents,getStudentById,getStudentsDetailsWithFilters,updateStudents,updateStudentsStatus} from '../controllers/stucontrollers.js';
 
-router.get("/get-stu", getstudents);
-router.post("/add-user",addstudents);
+router.get('/get-students', getStudentsDetails);
+router.post('/add-students', addStudents);
+router.get('/get-student-byid/:userid', getStudentById);//params single
+router.get('/get-std-details-withfilter', getStudentsDetailsWithFilters);//query parameters
+router.put("/update-students/:id", updateStudents);
+router.put("/update-students-status", updateStudentsStatus);
+//Testing API -> "http://localhost:2885/get-std-details-withfilter?stdBranch=MECH&stdCollege=AUS&stdCgpa=7.0"
+
 export default router;
+
+
